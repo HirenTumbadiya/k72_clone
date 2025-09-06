c31f9cab82a4d66bfe27c06a6507b7673c209ddd
HirenTumbadiya <tumbadiyahiren@gmail.com>
2025-09-06 17:37:54 +0530

    updated agence code

--- src/components/home/HomeHeroText.tsx
+++ src/components/home/HomeHeroText.tsx
@@ -3,15 +3,21 @@ import Video from "./video";
 export default function HomeHeroText() {
   return (
     <div className="font-[lausanne-300] pt-5 text-center">
-      <div className="text-[9vw] flex items-center justify-center uppercase leading-[8vw]">L'étincelle</div>
       <div className="text-[9vw] flex items-center justify-center uppercase leading-[8vw]">
+        L'étincelle
+      </div>
+      <div className="text-[9vw] flex items-center justify-center uppercase leading-[8vw] overflow-hidden">
         qui
         <div className="h-[7vw] w-[16vw] rounded-full overflow-hidden -mt-5">
-          <Video />
+          <div className="h-[7vw] w-[16vw] rounded-full overflow-hidden">
+            <Video />
+          </div>
         </div>
         génère
       </div>
-      <div className="text-[9vw] flex items-center justify-center uppercase leading-[8vw]">la créativité</div>
+      <div className="text-[9vw] flex items-center justify-center uppercase leading-[8vw]">
+        la créativité
+      </div>
     </div>
   );
 }
--- src/pages/Agence.tsx
+++ src/pages/Agence.tsx
@@ -1,11 +1,27 @@
 export default function Agence() {
   return (
-    <div className="font-[lausanne-500]">
-      <div className="mt-[50vh]">
-        <h1 className="text-[18vw] uppercase leading-[18vw] text-center">
-          Soixan7e <br />
-          Douze
-        </h1>
+    <div>
+      <div className="h-[20vw] w-[15vw] rounded-3xl absolute top-40 left-[30vw] overflow-hidden">
+        <img src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg" alt="Agence" className="w-full h-full" />
+      </div>
+      <div className="relative font-[lausanne-500]">
+        <div className="mt-[50vh]">
+          <h1 className="text-[18vw] uppercase leading-[18vw] text-center">
+            Soixan7e <br />
+            Douze
+          </h1>
+        </div>
+        <div className="pl-[40%] mt-20">
+          <p className="text-6xl">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            Notre curiosité nourrit notre créativité. On reste humbles et on dit
+            non aux gros egos, même le vôtre. Une marque est vivante. Elle a des
+            valeurs, une personnalité, une histoire. Si on oublie ça, on peut
+            faire de bons chiffres à court terme, mais on la tue à long terme.
+            C&apos;est pour ça qu&apos;on s&apos;engage à donner de la
+            perspective, pour bâtir des marques influentes.
+          </p>
+        </div>
       </div>
     </div>
   );
